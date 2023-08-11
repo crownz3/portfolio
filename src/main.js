28b6fa3272b71907ba3064db50427ea057e7af4b
Mohamed Ibrahim <crowwnzz3@gmail.com>
2023-08-11 15:34:25 +0530

    Add a dark mode

--- src/main.js
+++ src/main.js
@@ -9,7 +9,26 @@ import '../styles/components/contact.css';
 import '../styles/components/footer.css';
 import '../styles/components/mobile-nav.css';
 import '../styles/utils.css';
-
 import mobileNavBar from './utils/mobile-nav';
+import darkMode from './utils/dark-mode'
 
 mobileNavBar();
+darkMode()
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const arrowBtn = document.querySelector('.body__arrow')
+    window.addEventListener('scroll', () => {
+        const scrollPercentage = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100
+        if (scrollPercentage > 20) {
+            arrowBtn.style.display = "block";
+        } else {
+            arrowBtn.style.display = "none";
+        }
+
+        arrowBtn.addEventListener("click", function () {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        });
+    })
+});
+
---
+++ src/utils/dark-mode.js
@@ -0,0 +1,34 @@
+const darkMode = ()=>{
+const themeToggleBtns = document.querySelectorAll('#theme-toggle')
+
+// State
+const theme = localStorage.getItem('theme');
+
+// On Mount
+theme &&  document.body.classList.add(theme)
+
+// Handlers
+const handleThemeToggle = () => {
+    themeToggleBtns.forEach(btn =>{
+        btn.addEventListener('click',()=>{
+            document.body.classList.toggle('light-mode');
+            if(document.body.classList.contains('light-mode')){
+            localStorage.setItem("theme","light-mode");
+    
+            }else {
+                localStorage.removeItem('theme')
+                document.body.removeAttribute('class')
+            }
+        })
+    })
+}
+
+// Events
+themeToggleBtns.forEach(btn =>{
+    btn.addEventListener('click',()=> handleThemeToggle())
+})
+
+
+}
+
+export default darkMode;
